326c571cf440382bd4f9fcc001f49f3825a91781
3Emme <emmettbuentiempo@gmail.com>
2020-10-16 10:18:41 -0700

    use lazyloading onconfiguring file

--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -4,9 +4,13 @@ namespace Factory.Models
 {
   public class FactoryContext : DbContext
   {
-    public virtual DbSet<Engineer> Engineers { get; set; }
+    public DbSet<Engineer> Engineers { get; set; }
     public DbSet<Machine> Machines { get; set; }
 
     public FactoryContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
   }
 }
