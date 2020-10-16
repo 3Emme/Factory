3843d80be1155045d3e6c6f192e71d5a47269af8
3Emme <emmettbuentiempo@gmail.com>
2020-10-16 13:29:40 -0700

    update to reflect addition of EngineerMachine join model

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -4,9 +4,14 @@ namespace Factory.Models
 {
   public class Engineer
   {
+    public Engineer()
+    {
+      this.Machines = new HashSet<EngineerMachine>();
+    }
     public int EngineerId { get; set; }
     public string EngineerName { get; set; }
     public string EngineerDescription { get; set; }
     public string EngineerStatus { get; set; }
+    public virtual ICollection<EngineerMachine> Machines { get; set; }
   }
 }
