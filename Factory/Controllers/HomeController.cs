e2777a22620188c7fc836935ee6debe2bca7be4b
3Emme <emmettbuentiempo@gmail.com>
2020-10-16 17:11:26 -0700

    refactor to facilitate display of both engineers and machines index on home index view

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,14 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
-
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db ;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      List<Engineer> engineersModel = _db.Engineers.ToList();
+      List<Machine> machinesModel = _db.Machines.ToList();
+      model.Add("engineers", engineersModel);
+      model.Add("machines", machinesModel);
+      return View(model);
     }
   }
 }
